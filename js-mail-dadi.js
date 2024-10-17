53e15b0a68ba1150c2019307234b583f021a801a
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-17 15:36:55 +0200

    add

---
+++ js-mail-dadi.js
@@ -0,0 +1,23 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.*/
+const emailList = ["esempio1@hotmail.it", "esempio2@gmail.com, esempio3@outlook.com"];
+const userMail = prompt("Inserisci la tua email");
+let text="";
+if (emailList.includes(userMail)) {
+    text="Accesso consentito";
+} else {
+    text="la sua email non è tra quelle autorizzate ad accedere";
+}
+// console.log(text);
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+const player = Math.floor(Math.random()*6)+1;
+const computer = Math.floor(Math.random()*6)+1;
+let text2 = "Parità";
+if (player>computer) {
+    text2 = "Hai vinto"
+} else if (computer>player) {
+    text2 = "Hai perso"
+}
+console.log(`Il tuo punteggio: ${player}\nIl punteggio del computer: ${computer}\nRISULTATO: ${text2}`);
